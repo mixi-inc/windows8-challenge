613dd40f7a6a9390a5a6135a0d2f64fee6c2f265
genichiro.sugawara <genichiro.sugawara@mixi.co.jp>
2012-11-16 12:13:27 +0900

    done

---
+++ App1/App1/js/model/photo.js
@@ -0,0 +1,30 @@
+﻿// ■依存
+// tokenManager.js
+// APIRequester.js
+
+(function () {
+    "use strict";
+
+    /**
+     * かんたん公開にアップされているフォトの情報を取得します
+     *
+     * @name getAsync
+     * @param なし
+     * @return {WinJS.Promise} 処理のPromiseオブジェクト
+     */
+    function getAsync() {
+
+        return TokenManager.getTokenAsync()
+        .then(function (token) {
+            return APIRequester.getDefaultAlbumPhotos(token);
+        })
+        .then(function (result) {
+            // TODO: resultに入っているレスポンスを整形してdummyと同じフォーマットにしましょう
+            return [];
+        });
+    }
+
+    WinJS.Namespace.define("PhotoModel", {
+        getAsync: getAsync
+    });
+})();
---
+++ App1/App1/js/pages/view.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    var page = WinJS.UI.Pages.define("/html/view.html", {
+
+        ready: function (element, options) {
+            var listView = itemList.winControl;
+
+            WinJS.UI.setOptions(listView, {
+                itemTemplate: simple_ItemTemplate,
+                itemDataSource: new WinJS.Binding.List(DummyModel.get()).dataSource,
+                layout: new WinJS.UI.GridLayout()
+            });
+        }
+    });
+})();
